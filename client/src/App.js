0488f02258d069e5461f08f58756e85f26469e31
Kristian Fulkerson <kristianf451@gmail.com>
2022-05-14 14:21:14 -0700

    feat: :alien: added dependencies axios,react-router-dom,tailwindcss. Testing tailwind, working and connected

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
-function App() {
+import React from "react";
+
+const App = () => {
   return (
-    <div>
-      <h1>Test</h1>
+    <div className="min-h-screen max-w-7xl w-full flex justify-center items-center">
+      <h1 className="text-green-500 font-extrabold underline text-3xl">Test</h1>
     </div>
   );
-}
+};
 
 export default App;
