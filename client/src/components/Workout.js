582e40eeb35806691eb92f745474bba0b1f064cc
Kristian Fulkerson <kristianf451@gmail.com>
2022-05-16 16:46:51 -0700

    WIP: :trollface: working on styling out the individual workout component, added some animations on the image hover as well as a delete, edit, and like icon

--- client/src/components/Workout.js
+++ client/src/components/Workout.js
@@ -5,15 +5,38 @@ import { BiDotsHorizontalRounded } from "react-icons/bi";
 
 const Workout = ({ workout }) => {
   return (
-    <div>
-      <h1>{workout.title}</h1>
-      <img
-        src={workout.selectedFile}
-        alt="workout"
-        className="w-full object-contain h-56"
-      />
-      <h3>{workout.creator}</h3>
+    <div className="max-h-96 flex flex-col justify-center items-center border border-black rounded-md p-2 cursor-pointer">
+      <figure className="relative">
+        <div className="flex items-center justify-between w-full">
+          <div>
+            <h1>{workout.title}</h1>
+            <h3>By: {workout.creator}</h3>
+          </div>
+          <BiDotsHorizontalRounded size={25} className="mb-4" />
+        </div>
+
+        <div className="overflow-hidden">
+          <img
+            src={workout.selectedFile}
+            alt="workout"
+            className="w-full object-contain h-56 transition-all duration-1000 ease-in-out hover:scale-110 hover:opacity-90"
+          />
+        </div>
+        <figcaption className="absolute bottom-0 left-0 bg-white w-full opacity-80 flex justify-center py-2">
+          {workout.workoutType}
+        </figcaption>
+      </figure>
       <p>{workout.description}</p>
+      <div className="flex justify-between w-full">
+        <FiThumbsUp
+          size={25}
+          className="transition-transform duration-500 ease-in-out hover:translate-y-0.5 cursor-pointer"
+        />
+        <AiFillDelete
+          size={25}
+          className="transition-transform duration-500 ease-in-out hover:translate-y-0.5 cursor-pointer"
+        />
+      </div>
     </div>
   );
 };
