8c735244f6bb6a01fe6ef3b30192c23fde36f40a
Kristian Fulkerson <kristianf451@gmail.com>
2022-05-16 17:54:19 -0700

    feat: :alien: added webkit package to fix blur on images after scaling effect. Added moment as a FE dependency for working with our createdAt. WIP

--- client/src/components/Workout.js
+++ client/src/components/Workout.js
@@ -2,6 +2,7 @@ import React from "react";
 import { FiThumbsUp } from "react-icons/fi";
 import { AiFillDelete } from "react-icons/ai";
 import { BiDotsHorizontalRounded } from "react-icons/bi";
+import moment from "moment";
 
 const Workout = ({ workout }) => {
   return (
@@ -10,7 +11,9 @@ const Workout = ({ workout }) => {
         <div className="flex items-center justify-between w-full">
           <div>
             <h1>{workout.title}</h1>
-            <h3>By: {workout.creator}</h3>
+            <h3>
+              By: {workout.creator} - {moment(workout.createdAt).fromNow()}
+            </h3>
           </div>
           <BiDotsHorizontalRounded size={25} className="mb-4" />
         </div>
@@ -19,7 +22,7 @@ const Workout = ({ workout }) => {
           <img
             src={workout.selectedFile}
             alt="workout"
-            className="w-full object-contain h-56 transition-all duration-1000 ease-in-out hover:scale-110 hover:opacity-90"
+            className="opacity-80 w-full object-contain h-56 transition-all duration-1000 linear hover:scale-125 hover:opacity-100"
           />
         </div>
         <figcaption className="absolute bottom-0 left-0 bg-white w-full opacity-80 flex justify-center py-2">
