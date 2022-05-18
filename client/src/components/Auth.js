4246144e53d5ec63541947172cc65c3bdfa0d6a5
Kristian Fulkerson <kristianf451@gmail.com>
2022-05-17 17:30:14 -0700

    feat: make mock form to add a user to our database, WIP

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -9,14 +9,28 @@ const Auth = () => {
     e.preventDefault();
   };
 
-  const handleChange = (e) => {};
+  const handleChange = (e) => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const clear = () => {
+    setFormValues({
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    });
+  };
   return (
     <div className="flex justify-center items-center min-h-screen w-full">
-      <div className="bg-red-500">
-        <div className="flex flex-col justify-center items-center w-full">
+      <div className="w-[400px] mx-auto bg-gray-300 rounded-lg p-4 shadow-lg">
+        <div className="flex flex-col justify-center items-center bg-red-500 rounded-full p-2">
           <AiFillLock size={25} className="" />
-          <h2>{isSignup ? "Sign Up" : "Sign In"}</h2>
         </div>
+        <h2 className="text-center">{isSignup ? "Sign Up" : "Sign In"}</h2>
 
         <form onSubmit={handleSubmit}>
           <div className="grid grid-cols-12 gap-2 p-4">
@@ -26,6 +40,8 @@ const Auth = () => {
                   type="text"
                   name="firstName"
                   label="First Name"
+                  value={formValues.firstName}
+                  placeholder="First Name"
                   handleChange={handleChange}
                   half
                   autoFocus
@@ -34,12 +50,46 @@ const Auth = () => {
                   type="text"
                   name="lastName"
                   label="Last Name"
+                  value={formValues.lastName}
+                  placeholder="Last Name"
                   handleChange={handleChange}
                   half
                 />
-                <AuthInput type="password" name="password" />
-                <AuthInput type="text" />
-                <AuthInput type="text" />
+                <AuthInput
+                  type="email"
+                  name="email"
+                  label="Email"
+                  placeholder="Email"
+                  value={formValues.email}
+                  handleChange={handleChange}
+                />
+                <AuthInput
+                  type="password"
+                  name="password"
+                  label="Password"
+                  placeholder="Example: abc123"
+                  value={formValues.password}
+                  handleChange={handleChange}
+                />
+                <AuthInput
+                  type="password"
+                  name="confirmPassword"
+                  label="Confirm Password"
+                  placeholder="abc123"
+                  value={formValues.confirmPassword}
+                  handleChange={handleChange}
+                />
+
+                <button type="submit" className="bg-white h-12 w-24 rounded-md">
+                  Submit
+                </button>
+                <button
+                  type="button"
+                  className="bg-gray-400 h-12 w-24 rounded-md ml-20"
+                  onClick={clear}
+                >
+                  Clear
+                </button>
               </>
             )}
           </div>
--- client/src/components/AuthInput.js
+++ client/src/components/AuthInput.js
@@ -1,37 +1,33 @@
 import React from "react";
 import { MdVisibility, MdVisibilityOff } from "react-icons/md";
 
-const AuthInput = ({ half, name, type, autoFocus, label, handleChange }) => {
-  const toggleShowPassword = () => {
-    if (type === "password") {
-      type = "text";
-    } else {
-      type = "password";
-    }
-  };
+const AuthInput = ({
+  half,
+  name,
+  value,
+  type,
+  autoFocus,
+  label,
+  handleChange,
+  placeholder,
+}) => {
   return (
-    <div
-      className={`relative col-span-12 ${
-        half ? "sm:col-span-6" : "sm:col-span-12"
-      }`}
-    >
-      <label
-        htmlFor={label}
-        className="block text-gray-700 text-sm font-bold mb-2"
-      ></label>
+    <div className={`relative col-span-12 ${half ? "sm:col-span-6" : ""}`}>
+      <label htmlFor={label} className="block text-sm font-bold mb-2">
+        {label}
+      </label>
       <input
         type={type}
+        value={value}
         name={name}
+        placeholder={placeholder}
         onChange={handleChange}
         className="h-12 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none"
         autoFocus={autoFocus}
         required
       />
-      {name === "password" ? (
-        <div
-          className="absolute z-10 bottom-0 left-[23.5rem] top-5"
-          onClick={toggleShowPassword}
-        >
+      {name === "password" || name === "confirmPassword" ? (
+        <div className="absolute z-10 bottom-0 left-[18.5rem] top-10">
           {type === "password" ? (
             <MdVisibility size={25} />
           ) : (
